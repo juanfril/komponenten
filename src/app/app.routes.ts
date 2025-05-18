91e60c44cbf33ae241a10d41095ab1062000a767
juanfri <juanfril@gmail.com>
2025-05-18 08:43:55 +0200

    Refactor routing structure by lazy loading product routes. Moved product-related routes to a new file for better organization and maintainability.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,10 @@
 import { Routes } from '@angular/router';
 
-import { ProductDetailComponent } from './features/product/pages/product-detail/product-detail.component';
-import { ProductFormComponent } from './features/product/pages/product-form/product-form.component';
-import { ProductListComponent } from './features/product/pages/product-list/product-list.component';
-
 export const routes: Routes = [
   { path: '', redirectTo: '/products', pathMatch: 'full' },
-  { path: 'products', component: ProductListComponent },
-  { path: 'products/new', component: ProductFormComponent },
-  { path: 'products/:id', component: ProductDetailComponent },
-  { path: 'products/edit/:id', component: ProductFormComponent },
+  {
+    path: 'products',
+    loadChildren: () => import('./features/product/product.routes').then(m => m.PRODUCT_ROUTES),
+  },
   { path: '**', redirectTo: '/products' },
 ];
